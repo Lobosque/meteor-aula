2fec25209be1acb3fbdf9d3261bb5b7fa030c7a5
Lucas Lobosque <lucaslobosque@gmail.com>
2014-03-15 15:23:53 -0300

    usuarios e cabeçalho

--- client/views/posts_list.js
+++ client/views/posts_list.js
@@ -1,18 +1,3 @@
-var postsData = [
-  {
-    title: 'Goodbye Popcorn Time',
-    url: 'https://medium.com/p/93f890b8c9f4',
-  },
-  {
-    title: 'TunelBear vpn app',
-    url: 'https://www.tunnelbear.com/',
-  },
-  {
-    title: 'Update on Metro',
-    url: 'https://blog.mozilla.org/futurereleases/2014/03/14/metro/',
-  },
-];
-
 Template.postsList.helpers({
   posts: function() {
     return Posts.find();
